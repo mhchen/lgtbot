51c21c17df64e6aeb4bdef383e9a89a68c09b166
Mike Chen <1325549+mhchen@users.noreply.github.com>
2025-04-11 23:57:10 -0700

    Don't restart pm2 on db changes

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -2,6 +2,7 @@ module.exports = {
   name: 'lgtbot',
   script: 'src/index.ts',
   interpreter: 'bun',
+  ignore_watch: ['*.db'],
   env: {
     PATH: `${process.env.HOME}/.bun/bin:${process.env.PATH}`,
   },
