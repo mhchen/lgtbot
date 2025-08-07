68ebf332ce86202ffdc3284ae7b5ae8f2da07482
Walter Furrer <walterfurrer.dev@gmail.com>
2025-08-07 18:39:56 -0400

    Made changes per our Glorious Leaders instructions.

--- src/ryan-astro-ping.ts
+++ src/ryan-astro-ping.ts
@@ -5,25 +5,15 @@ const WATERCOOLER_CHANNEL_ID = '964903664265359433';
 const RYAN_USER_ID = '219283881390637056';
 
 export async function registerRyanAstroShillListeners(client: Client) {
-  client.on('messagesCreate', async (message) => {
+  client.on('messageCreate', async (message) => {
     if (message.author.bot || message.author.id === RYAN_USER_ID) {
       return;
     }
 
-    if (message.content.toLowerCase().includes('astro')) {
-      const targetChannel = client.channels.cache.get(
-        WATERCOOLER_CHANNEL_ID
-      ) as TextChannel;
-
-      if (!targetChannel) {
-        console.error('Target channel not found');
-        return;
-      }
-      const messageLink = `https://discord.com/channels/${message.guild?.id}/${message.channel.id}/${message.id}`;
-
-      await targetChannel.send(
-        `<@${RYAN_USER_ID}, your sevices as Astro Shill are required here: ${messageLink}.`
-      );
+    if (/\bastro\b/i.test(message.content)) {
+      await message.reply({
+        content: `<@${RYAN_USER_ID}>, your services as Astro Shill are required.`,
+      });
     }
   });
 
